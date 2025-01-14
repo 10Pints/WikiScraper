55bf3925d16467491403d53c7617b704cf2aeca7
10Pints <40173667+10Pints@users.noreply.github.com>
2025-01-15 00:02:54 +0800

    Initial load
    Initial load

--- WikiScraper.py
+++ WikiScraper.py
@@ -5,109 +5,153 @@ import csv
 import os
 from bs4 import BeautifulSoup# as bs
 import re
-# pip pyodbc
 import pyodbc as pyodbc
 import pandas as pd
-#import wikipediaapi as api
 
 conn = pyodbc.connect( 'DRIVER={SQL Server};'
                        'SERVER=DevI9;'
                        'DATABASE=farming_dev;Trusted_Connection=yes;'
                     )
+
 cursor = conn.cursor()
 
 #with named parameters: cursor.execute('EXEC usp_get_user_data @name = ?, @username = ?', 'tim', 'flipperpa')
-cmd_prod_executesp = """EXEC dbo.sp_list_AppLog"""
 conn.autocommit = True
-
-#row = cursor.execute(cmd_prod_executesp)
-cursor.execute('Select pathogen_nm,latin_nm FROM pathogen')
+cursor.execute('Select pathogen_id, pathogen_nm,latin_nm FROM pathogen')
 rows = cursor.fetchall()
 
-#Create a map of pathogen_nm to url
-d = {}
-pathogen_nm = ""
-pathogen_nm2 = ""
-latin_nm = ""
-url = ""
-key = ''
-val = ''
 i = 0
+image_path = ''
+key = ''
+latin_nm = ''
+ndx = 0
+pathogen_nm = ''
+pathogen_nm2 = ''
+url = ''
+val = ''
+taxonomy = ''
+status_msg = ''
+wanted_keys = ['Domain','Kingdom','Clade','Phylum','Division','Class','Order','Infraorder','Family','','Subfamily','Genus','Section']
+
 # open a file to hold the references found
-f = open("pathogens_wiki_urls.csv", "w")
-f.write(f"pathogen_nm,url\timage")
+f = open('pathogens_wiki_urls.txt', 'w')
+f.write(f'id\pathogen_nm\turl\timage\ttaxonomy\tstatus\n')
 
 for row in rows:
-    i=i+1
-    url=''
-    quote = {}
-    domain = ''
-
-    try:
-        pathogen_nm = row[0] #pathogen_nm = "Alternaria"    
-        latin_nm = row[1]
-        srch_key = latin_nm if latin_nm is not None else pathogen_nm
-        srchResults = w.search(srch_key)
-        srch_res = srchResults[0]
-        page = w.page(srch_res, auto_suggest=False) # may throw 
-        url = page.url
-        content=page.content
-        summary=page.summary
-        sections=page.sections
-        images=page.images
-        soup = BeautifulSoup(page.html(), 'html5lib')
-        pretty = soup.prettify()
-        #
-        table = soup.find('table', attrs = {'class':'infobox biota'}) 
-        trs=table.findAll('tr')
-
-        for tr in trs:
-            contents = tr.contents
-
-            if len(contents)>3:
-                key = contents[1].contents
-                kingdom = contents[3].text.strip()
-                domain = tr.find(string=re.compile('Domain')).text.strip()
-
-        image = page.images[0]
-
-    except w.exceptions.PageError as error:
-        print(f'caught page error ex: {error}')
-
-    except Exception as error:
-        print(f'caught ex: {type(error).__name__}: {error}')
+    i=row[0]
+    quote   = {}
+    url     =''
+    status_msg = ''
+    taxonomy = ''
+    image_path = ''
+    
+    while True:
         try:
-            print(f"[{i}]: [{pathogen_nm}] has no page in wikipedia trying latin name [{latin_nm}]")
-            srchResults = w.search(pathogen_nm)
+            pathogen_nm = row[1] #pathogen_nm = "Alternaria"    
+            latin_nm = row[2]
+            srch_key = latin_nm if latin_nm is not None else pathogen_nm
+            srchResults = w.search(srch_key)
+            srch_res = srchResults[0]
+
+            # This needs restoring to the old way of doing it and handling the duplicate exception
+            page = w.page(srch_res, auto_suggest=False) # may throw 
+            url = page.url
+            content=page.content
+            summary=page.summary
+            sections=page.sections
+            images=page.images
+
+            if len(images) > 0:
+                image_path = images[0] 
+            else :
+                image_path = None
+
+            soup = BeautifulSoup(page.html(), 'html5lib')
+            table = soup.find('table', attrs = {'class':'infobox biota'}) 
+
+            if table == None and latin_nm != None:
+                status_msg = f"[{i}]: There is no taxonomy table in the wiki page for {latin_nm} - trying common nm: {pathogen_nm}"
+                print(status_msg)
+                srchResults = w.search(pathogen_nm)
+                srch_res = srchResults[0]
+
+                # try the common name
+                page = w.page(srch_res, auto_suggest=False) # may throw 
+                url = page.url
+                soup = BeautifulSoup(page.html(), 'html5lib')
+                table = soup.find('table', attrs = {'class':'infobox biota'}) 
+
+                if table == None:
+                    status_msg = f"[{i}]: There is no taxonomy table in the wiki page for either the latin name {latin_nm} or the common nm: {pathogen_nm}"
+                    print(status_msg)                
+                    break
+
+            #------------------------------------------------
+            # ASSERTION: if here then we have the tax table
+            #------------------------------------------------
+            tds=table.findAll('td')
+            ndx = 0
+            len_tr = len(tds)
+    
+            for n in range (1, len_tr, 2):
+                key = tds[n].text.strip().strip(':')
+
+                if any(key in x for x in wanted_keys):
+                    val = tds[n+1].text.strip()
+                    taxonomy = f'{taxonomy},{key}:{val}'
+
+            print(f'taxonomy: {taxonomy}')
+            break
+    
+        except w.exceptions.PageError as error:
+            status_msg =f'[{i}]: caught page error ex: {error}'
+            print(status_msg)
+            break
+
+        except Exception as error:
+            status_msg =f'[{i}]: caught ex: {type(error).__name__}: {error} ndx: {ndx}'
+            print(status_msg)
 
             try:
-                if pathogen_nm in srchResults:
-                    ndx = srchResults.index(pathogen_nm)
-                    page = w.page(srchResults[ndx])
-                    url = page.url
-                else:
-                    # Try a trim
-                    pathogen_nm2 = pathogen_nm.rstrip('s')
+                status_msg = f'{status_msg}.\n[{pathogen_nm}] has no page in wikipedia trying latin name [{latin_nm}]'
+                print(status_msg)
+                srchResults = w.search(latin_nm)
 
-                    if pathogen_nm2 in srchResults:
-                        ndx = srchResults.index(pathogen_nm2)
-                        page = w.page(pathogen_nm2)
+                try:
+                    if pathogen_nm in srchResults:
+                        ndx = srchResults.index(pathogen_nm)
+                        page= w.page(srchResults[ndx])
                         url = page.url
                     else:
-                        print(f"[{i}]: problem [{pathogen_nm}] or [{latin_nm}]")
-            except Exception as error:
-                print(f'caught ex: {type(error).__name__}')
-                page = w.page(latin_nm)
-                #print(f"[{i}]: {latin_nm}\t{page.url}\t{image}")
-                url = page.url
-        except:
-            print(f"[{i}]: There is no reference in wikipedia for either [{pathogen_nm}] or [{latin_nm}]")
-            continue
+                        # Try a trim
+                        pathogen_nm2 = pathogen_nm.rstrip('s')
 
-    #srchResults = w.search(latin_nm)
-    print(f"[{i}]: {pathogen_nm}\t{url}\t{image}")
-    d[pathogen_nm] = url
-    f.write(f"{pathogen_nm},{url}\t{image}")
+                        if pathogen_nm2 in srchResults:
+                            ndx = srchResults.index(pathogen_nm2)
+                            page= w.page(pathogen_nm2)
+                            url = page.url
+                        else:
+                            status_msg = f'{status_msg}\nproblem wiki page not found for common nm [{pathogen_nm}] or latin name: [{latin_nm}]'
+                            print(status_msg)
+
+                except Exception as error:
+                    status_msg = f'{status_msg}.\ncaught ex: {type(error).__name__}'
+                    print(status_msg)
+                    page = w.page(latin_nm)
+                    url = page.url
+            except:
+                status_msg = f"[{i}]: There is no reference in wikipedia for either [{pathogen_nm}] or [{latin_nm}]"
+                print(status_msg)
+                break
+            
+        taxonomy = taxonomy.strip(',')
+        status_msg = 'OK'
+        break #do while once loop
+
+    line = f"{i}\t{pathogen_nm}\t{url}\t{image_path}\t{taxonomy}\t{status_msg}"
+    print(f'[{i}]: {line}')
+    line = f"{line}\n"
+    f.write(line)
     f.flush()
 
 conn.close()
